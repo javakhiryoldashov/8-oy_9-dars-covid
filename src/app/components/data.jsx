4b9795df70576fe9ea29c3e7978ba861e8c6b690
javokhiryoldashov <yuldoshovjavohir3@gmail.com>
2024-08-08 02:28:54 +0500

    fenish

---
+++ src/app/components/data.jsx
@@ -0,0 +1,33 @@
+"use client"
+
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from "chart.js";
+import { Data, Options } from "./search";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+);
+
+const LineChart = () => {
+  return <Line data={Data} options={Options} />;
+};
+
+export default LineChart;
